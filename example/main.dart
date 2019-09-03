9c7d236a288c12e4fbcf9b17feea2b4c4f7ab5a0
Tobe O <thosakwe@gmail.com>
2019-09-03 16:49:38 -0400

    Hello world works

--- example/main.dart
+++ example/main.dart
@@ -2,7 +2,8 @@ import 'package:flurses/flurses.dart';
 import 'package:flurses/flurses_io.dart';
 
 main() {
-  runApp(HelloApp());
+  // TODO: Remove the need to disable resetting
+  runApp(HelloApp(), resetAfterExit: false);
 }
 
 class HelloApp extends StatelessWidget {
--- lib/flurses_io.dart
+++ lib/flurses_io.dart
@@ -3,13 +3,26 @@ import 'dart:io';
 import 'package:flurses/durses_io.dart';
 import 'package:flurses/flurses.dart';
 
+// TODO: Docs
 Future<void> runApp(Widget app,
-    {Stdin stdin, Stdout stdout, bool listenForResize = true}) async {
+    {Stdin stdin,
+    Stdout stdout,
+    bool listenForResize = true,
+    bool lineMode = false,
+    bool echoMode = false,
+    bool clear = true,
+    bool resetAfterExit = true}) async {
   var terminal = IoTerminal(stdin: stdin, stdout: stdout);
-  var runner = Runner(
-    terminal,
-    app,
-    onResize: listenForResize ? ProcessSignal.sigwinch.watch() : null,
-  );
-  return runner.run();
+  return terminal.run(() {
+    var runner = Runner(
+      terminal,
+      app,
+      onResize: listenForResize ? ProcessSignal.sigwinch.watch() : null,
+    );
+    return runner.run();
+  },
+      lineMode: lineMode,
+      echoMode: echoMode,
+      clear: clear,
+      resetAfterExit: resetAfterExit);
 }
