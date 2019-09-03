7814be5dfffb51a7ffa1ba4dcbafe4c969d95937
Tobe O <thosakwe@gmail.com>
2019-09-03 09:37:04 -0400

    reinit

--- lib/flurses.dart
+++
@@ -1,7 +0,0 @@
-export 'dart:async' show Future;
-export 'package:meta/meta.dart' show immutable, required;
-export 'src/ui/ui.dart';
-export 'src/key.dart';
-export 'src/renderer.dart' hide Renderer;
-export 'src/run_app.dart';
-export 'src/widget.dart';
--- lib/src/ui/stdin_listener.dart
+++
@@ -1,2 +0,0 @@
-import 'dart:io';
-import 'package:flurses/flurses.dart';
