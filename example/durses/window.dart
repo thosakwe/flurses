c5e6b7981afc6d1fa980fd26b3f1beba49a6e4f2
Tobe O <thosakwe@gmail.com>
2019-09-03 14:54:27 -0400

    comments in example

--- example/durses/window.dart
+++ example/durses/window.dart
@@ -1,11 +1,15 @@
 import 'package:flurses/durses.dart';
 import 'package:flurses/durses_io.dart';
 
+/// Analogous to "windows" in `curses` are `BoundedTerminal`
+/// instances.
+///
+/// They draw at an offset within a parent terminal.
 main() {
   var io = IoTerminal();
   return io.run(() async {
-    var wnd = BoundedTerminal(io, yOffset: 2);
-    wnd.writeString(0, 0, "This is actually line 2, not 0.");
+    var wnd = BoundedTerminal(io, yOffset: 2, xOffset: 3);
+    wnd.writeString(0, 0, "This is actually line 2, col 3, not (0, 0).");
     wnd.writeString(0, 1, "Press any key to exit.");
     wnd.readKey();
   });
